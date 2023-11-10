dcc6430f56062e34e4b36d071f4e8a6c23fc6f29
eric-merritt <ericm2009@gmail.com>
2023-11-10 04:18:22 -0700

    renamed:    mini-message-board-project/app.config.js -> -> mini-message-board-project/ecosystem.config.js

--- mini-message-board-project/ecosystem.config.js
+++ mini-message-board-project/ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
        "host" : ["10.1.10.15"],
        "ref"  : "origin/master",
        "repo" : "git@github.com:eric-merritt/mini-message-board-project.git",
-       "path" : "/var/www/html/mini-message-board-project",
+       "path" : "/etc/nginx/sites-enabled/mini-message-board-project",
        "post-deploy" : "npm install"
     }
   }
